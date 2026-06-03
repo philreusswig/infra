da012d2e4db57ee2534913bf74f6d96e0c1bc339
Phil Reusswig <phil.reusswig@gmail.com>
2026-06-02 21:04:23 -0700

    Add python packages installation
    DESCRIPTION:
    - Add python to packages list
    - Add python packages list to shared
    - Add python packages install to os-setup.sh
    
    TEST:
    - Run bootstrap.sh without error

--- system/fedora/os-setup.sh
+++ system/fedora/os-setup.sh
@@ -9,7 +9,7 @@ set -e
 echo "📦 [Fedora] Upgrading package list..."
 sudo dnf check-update || true
 
-echo "📦 [Fedora] Installing shared packages..."
+echo "📦 [Fedora] Installing packages..."
 grep -v '^#' system/fedora/packages.txt | grep -v '^$' | xargs sudo dnf install -y
 
 echo "📦 [Fedora] Installing Rust and Cargo..."
@@ -18,3 +18,6 @@ cargo install cargo-binstall --locked
 
 echo "📦 [Fedora] Installing Jujutsu..."
 cargo binstall --strategies crate-meta-data jj-cli
+
+echo "📦 [Fedora] Installing Python packages..."
+python3 -m pip install -r system/shared/python-packages.txt
